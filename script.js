27c8e2da323542346a5d5185a3a212ed23917203
Eduardo Iriarte-Mendez <eduardo.iriarte-mendez@ratepay.com>
2021-04-23 23:25:31 +0200

    chore: implement loesung for task 11

--- script.js
+++ script.js
@@ -5,12 +5,28 @@ tetris.beiGestenSteuerungEreignis = function (geste) {
 
     // Hier kommt kein Code:
 
+    switch(geste.direction) {
+        case 'Long up':
+        case 'Up':
+            this.tetro.drehen(this.welt);
+            break;
+        case 'Long left':
+        case 'Left':
+            this.tetroNachLinksBewegen();
+            break;
+        case 'Long right':
+        case 'Right':
+            this.tetroNachRechtsBewegen();
+            break;
+        case 'Long down':
+        case 'Down':
+            this.tetroNachUntenSchieben();
+            break;
+        default:
+            console.log(geste.direction);
+    }
 
-
-
-
-
-
+    this.aktualisieren();
 
     // ----
 };
